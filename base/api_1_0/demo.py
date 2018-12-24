932d67b2f22be55490b3b0950c649aa8a03e2c75
layueyu <layueyu@zero.com>
2018-12-24 18:01:39 +0800

    demo

--- base/api_1_0/demo.py
+++ base/api_1_0/demo.py
@@ -2,3 +2,28 @@
 demo
 """
 
+from . import api
+from base.tasks.demo.tasks import demo, demo1, demo2
+from base.commons.response import R
+
+
+@api.route('/task/test')
+def task():
+    """无参数定时任务"""
+    demo.delay()
+    return R.ok('OK')
+
+
+@api.route('/task/test1')
+def task():
+    """无参数定时任务"""
+    demo1.delay('/task/test1')
+    return R.ok('OK')
+
+
+@api.route('/task/test1')
+def task():
+    """无参数定时任务"""
+    result = demo.delay()
+    return R.ok('OK', result)
+
